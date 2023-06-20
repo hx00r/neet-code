5ba36eac6a014d042bc33212d32a04f825fbb936
Hossam Mahmoud(hx00r) <mr.joker00200@gmail.com>
2023-06-20 22:18:09 +0300

    first commit of the 01.array.and.hashmap folder structure

---
+++ 01. Arrays & Hashing/01. Contains Duplicate.py
@@ -0,0 +1,10 @@
+# 217. Contains Duplicate 
+class Solution(object):
+    def containsDuplicate(self, nums) :
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(set(nums)) != len(nums):
+            return True
+        return False
